842226ae04c75239b501914d37da07cb24b3f1bf
Anthony Bobrov <anton.bobrov@hotmail.com>
2023-05-24 18:40:46 +0400

    ScrollView: optimize `changeProp`

--- apps/react-components/src/components/ScrollView/Provider.tsx
+++ apps/react-components/src/components/ScrollView/Provider.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useEffect, useMemo, useRef, useState } from 'react';
 import { ScrollView, vevet } from '@anton.bobrov/vevet-init';
 import { isIntersectionObserverSupported } from '@anton.bobrov/react-hooks';
+import { useDeepCompareMemoize } from 'use-deep-compare-effect';
 import { IScrollViewContext, ScrollViewContext } from './utils/context';
 import { IScrollViewProviderProps } from './types';
 
@@ -48,7 +49,8 @@ export const Provider: FC<IScrollViewProviderProps> = ({
 
   useEffect(() => {
     scrollView?.changeProp({ enabled: isEnabled, ...props });
-  }, [isEnabled, scrollView, props]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [scrollView, isEnabled, useDeepCompareMemoize(props)]);
 
   return (
     <ScrollViewContext.Provider value={contextValue}>
