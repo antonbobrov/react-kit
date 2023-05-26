7e35f7c4aae1277756a0d0c1b26012cd7a3ee79a
Anthony Bobrov <anton.bobrov@hotmail.com>
2023-05-26 20:50:13 +0400

    Deprecate `getDomLanguage`

--- apps/react-hooks/src/utils/dom/getDomLanguage.ts
+++ apps/react-hooks/src/utils/dom/getDomLanguage.ts
@@ -1,6 +1,9 @@
 import { isBrowser } from '../env/isBrowser';
 
-/** Get current language from `html` `lang` attribute */
+/**
+ * Get current language from `html` `lang` attribute
+ * @deprecated
+ */
 export function getDomLanguage() {
   if (!isBrowser) {
     return 'en';
