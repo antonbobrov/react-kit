c8766fab58a0ba777627751970fb56e4c6aa77e0
Anthony Bobrov <anton.bobrov@hotmail.com>
2023-06-20 23:09:31 +0400

    Fix docs build

--- build-docs.js
+++ build-docs.js
@@ -5,12 +5,3 @@ fs.cpSync('./apps/react-components/storybook-static/', './build-docs/', { recurs
 fs.cpSync('./apps/react-hooks/docs/', './build-docs/react-hooks/', { recursive: true });
 fs.cpSync('./apps/react-vevet-hooks/docs/', './build-docs/react-vevet-hooks/', { recursive: true });
 fs.cpSync('./apps/react-dat-gui/docs/', './build-docs/react-dat-gui/', { recursive: true });
-
-execSync('git add ./build-docs/');
-execSync('git commit -m "Build docs"');
-
-execSync('git subtree split --prefix build-docs -b docs-temp -d');
-execSync('git push -f --no-verify origin docs-temp:docs');
-execSync('git branch -D docs-temp');
-
-execSync('git reset --hard HEAD~1');
