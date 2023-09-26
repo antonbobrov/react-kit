aa565efa021c1630d5e608697c2ec2c97fa65db8
Anthony Bobrov <anton.bobrov@hotmail.com>
2023-09-26 12:56:15 +0400

    useAnimationFrame: add `onPlay`, `onPause`, `onFrame`, `autoFpsFrames` properties; remove `onRender` prop; return `frame`

--- apps/react-vevet-hooks/src/hooks/useAnimationFrame.ts
+++ apps/react-vevet-hooks/src/hooks/useAnimationFrame.ts
@@ -1,44 +1,72 @@
 import { useEvent } from '@anton.bobrov/react-hooks';
-import { useEffect, useRef, useState } from 'react';
-import { AnimationFrame } from '@anton.bobrov/vevet-init';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { AnimationFrame, NAnimationFrame } from '@anton.bobrov/vevet-init';
 
-export interface IUseAnimationFrameProps {
+export interface IUseAnimationFrameProps
+  extends Pick<NAnimationFrame.IChangeableProps, 'fps' | 'autoFpsFrames'> {
+  /** Event on animation play */
+  onPlay?: () => void;
+  /** Event on animation pause */
+  onPause?: () => void;
   /** Event on each frame */
-  onRender: () => void;
-  /** Frames per second */
-  fps?: number;
+  onFrame: () => void;
 }
 
 /** Create `vevet` `AnimationFrame` */
 export function useAnimationFrame({
-  onRender: onRenderProp,
-  ...props
+  onPlay: onPlayProp,
+  onPause: onPauseProp,
+  onFrame: onFrameProp,
+  fps,
+  autoFpsFrames,
 }: IUseAnimationFrameProps) {
   const [frame, setFrame] = useState<AnimationFrame | null>(null);
 
-  const initialProps = useRef(props);
+  const initialProps = useRef({ fps, autoFpsFrames });
 
-  const onRender = useEvent(onRenderProp);
+  const onPlay = useEvent(onPlayProp);
+  const onPause = useEvent(onPauseProp);
+  const onFrame = useEvent(onFrameProp);
 
   useEffect(() => {
     const instance = new AnimationFrame({
-      run: false,
       ...initialProps.current,
+      isEnabled: false,
     });
+
     setFrame(instance);
 
-    instance.addCallback('frame', onRender);
+    if (onPlay) {
+      instance.addCallback('play', onPlay);
+    }
+    if (onPause) {
+      instance.addCallback('pause', onPause);
+    }
+    instance.addCallback('frame', onFrame);
 
     return () => instance.destroy();
-  }, [onRender, initialProps]);
+  }, [onFrame, onPause, onPlay]);
 
   useEffect(() => {
-    frame?.changeProp({ fps: props.fps });
-  }, [frame, props?.fps]);
+    if (!frame) {
+      return;
+    }
 
-  const play = () => frame?.play();
+    const currentProps = frame.props;
+    console.log('try change props');
 
-  const pause = () => frame?.pause();
+    if (
+      currentProps.fps !== fps ||
+      currentProps.autoFpsFrames !== autoFpsFrames
+    ) {
+      console.log('change props');
+      frame.changeProps({ fps, autoFpsFrames });
+    }
+  }, [frame, fps, autoFpsFrames]);
 
-  return { play, pause };
+  const play = useCallback(() => frame?.play(), [frame]);
+
+  const pause = useCallback(() => frame?.pause(), [frame]);
+
+  return { frame, play, pause };
 }
