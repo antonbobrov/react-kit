568d27cbad03a8bdeb6070a89cd79cbf39a49274
Anthony Bobrov <anton.bobrov@hotmail.com>
2023-10-04 17:31:22 +0400

    Add `splitIntoChunks`

---
+++ apps/react-hooks/src/utils/array/splitIntoChunks.ts
@@ -0,0 +1,19 @@
+type TChunk<T> = {
+  key: number | string;
+  items: T[];
+};
+
+export function splitIntoChunks<T>(
+  source: T[],
+  chunkSize: number
+): TChunk<T>[] {
+  const chunks: TChunk<T>[] = [];
+
+  for (let i = 0; i < source.length; i += chunkSize) {
+    const chunk = source.slice(i, i + chunkSize);
+
+    chunks.push({ key: chunks.length, items: chunk });
+  }
+
+  return chunks;
+}
--- apps/react-hooks/src/utils/index.ts
+++ apps/react-hooks/src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './api';
+export * from './array';
 export * from './dom';
 export * from './env';
 export * from './math';
