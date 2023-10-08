a4ca58632c2c1d48a3da8a05ff20f62a66fdeced
Anthony Bobrov <anton.bobrov@hotmail.com>
2023-10-08 13:58:53 +0400

    Export `useDeepCompareMemoize` & `useDeepCompareEffectNoCheck`

--- apps/react-hooks/src/hooks/useDeepCompareEffect.ts
+++ apps/react-hooks/src/hooks/useDeepCompareEffect.ts
@@ -1,5 +1,12 @@
-import useDeepCompareEffect from 'use-deep-compare-effect';
+import useDeepCompareEffect, {
+  useDeepCompareMemoize,
+  useDeepCompareEffectNoCheck,
+} from 'use-deep-compare-effect';
 
 // This hook is reexported from `use-deep-compare-effect` for convenience.
 
-export { useDeepCompareEffect };
+export {
+  useDeepCompareEffect,
+  useDeepCompareMemoize,
+  useDeepCompareEffectNoCheck,
+};
