6633cca0b56a795a72069e80df1ce5c17fc90f9b
Anthony Bobrov <anton.bobrov@hotmail.com>
2024-09-24 16:31:03 +0400

    useLocalStorage: add `onLsChangeProp`

--- apps/react-hooks/src/hooks/useLocalStorage.ts
+++ apps/react-hooks/src/hooks/useLocalStorage.ts
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { isBrowser } from '@utils/env';
 import { useEventListener } from './useEventListener';
+import { useEvent } from './useEvent';
 
 /**
  * Use local storage state
@@ -8,6 +9,7 @@ import { useEventListener } from './useEventListener';
  * @param name Name of the storage
  * @param defaultValueProp Default value
  * @param hasServerState Has SSR. When SSR is used, set to false to prevent hydration errors
+ * @param onLsChangeProp Callback on local storage change
  *
  * @example
  *
@@ -28,11 +30,15 @@ export function useLocalStorage<T>(
   name: string,
   defaultValueProp: T,
   hasServerState = true,
+  onLsChangeProp?: (value: T) => void,
 ): [T, (value: T) => void] {
   const defaultValueRef = useRef(defaultValueProp);
 
+  const onLsChange = useEvent(onLsChangeProp);
+
   const getLsValue = useCallback(() => {
     let value: any;
+
     try {
       value =
         JSON.parse(localStorage.getItem(name) ?? 'null') ??
@@ -48,7 +54,12 @@ export function useLocalStorage<T>(
     hasServerState ? getLsValue() : defaultValueRef.current,
   );
 
-  useEffect(() => setStateValue(getLsValue()), [getLsValue]);
+  useEffect(() => {
+    const value = getLsValue();
+
+    setStateValue(value);
+    onLsChange?.(value);
+  }, [getLsValue, onLsChange]);
 
   const set = useCallback(
     (newValue: T) => {
@@ -62,7 +73,10 @@ export function useLocalStorage<T>(
     ref: isBrowser ? window : null,
     target: 'storage' as any,
     listener: () => {
-      setStateValue(getLsValue());
+      const value = getLsValue();
+
+      setStateValue(value);
+      onLsChange?.(value);
     },
   });
 
