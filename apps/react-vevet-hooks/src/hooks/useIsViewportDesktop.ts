eba4e5da1a227c55e8ef7bd8ffc9da3a57c13b1a
Anthony Bobrov <anton.bobrov@hotmail.com>
2024-10-04 13:03:57 +0400

    Update jsdoc

--- apps/react-vevet-hooks/src/hooks/useIsViewportDesktop.ts
+++ apps/react-vevet-hooks/src/hooks/useIsViewportDesktop.ts
@@ -1,5 +1,19 @@
 import { useBreakpointName } from './useBreakpointName';
 
+/**
+ * Custom React hook that checks if the current viewport breakpoint is 'desktop'.
+ *
+ * @example
+ * const MyComponent = () => {
+ *   const isDesktop = useIsViewportDesktop();
+ *
+ *   return (
+ *     <div>
+ *       {isDesktop ? 'Viewport is of desktop size.' : 'Not desktop'}
+ *     </div>
+ *   );
+ * };
+ */
 export function useIsViewportDesktop() {
   const breakpointName = useBreakpointName();
 
--- apps/react-vevet-hooks/src/hooks/useIsViewportTablet.ts
+++ apps/react-vevet-hooks/src/hooks/useIsViewportTablet.ts
@@ -1,5 +1,19 @@
 import { useBreakpointName } from './useBreakpointName';
 
+/**
+ * Custom React hook that checks if the current viewport breakpoint is 'tablet'.
+ *
+ * @example
+ * const MyComponent = () => {
+ *   const isTablet = useIsViewportTablet();
+ *
+ *   return (
+ *     <div>
+ *       {isTablet ? 'Viewport is of tablet size.' : 'Not tablet.'}
+ *     </div>
+ *   );
+ * };
+ */
 export function useIsViewportTablet() {
   const breakpointName = useBreakpointName();
 
