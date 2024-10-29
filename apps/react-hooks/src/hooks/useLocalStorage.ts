98f25b4d159f3d8e6dbffdd4a34878f615df451e
Anthony Bobrov <anton.bobrov@hotmail.com>
2024-10-30 03:30:04 +0400

    useLocalStorage: remove `hasServerState` prop

--- apps/react-hooks/src/hooks/useLocalStorage.ts
+++ apps/react-hooks/src/hooks/useLocalStorage.ts
@@ -12,7 +12,6 @@ import { useEvent } from './useEvent';
  *
  * @param name - The name of the local storage key.
  * @param defaultValueProp - The default value to use if there is no value in local storage.
- * @param hasServerState - Indicates if server-side rendering is used. Set to false to prevent hydration errors.
  * @param onLsChangeProp - Callback function to be called when local storage changes.
  *
  * @example
@@ -27,7 +26,6 @@ import { useEvent } from './useEvent';
 export function useLocalStorage<T>(
   name: string,
   defaultValueProp: T,
-  hasServerState = true,
   onLsChangeProp?: (value: T) => void,
 ): [T, (value: T) => void] {
   const defaultValueRef = useRef(defaultValueProp);
@@ -47,9 +45,7 @@ export function useLocalStorage<T>(
     return value;
   }, [name]);
 
-  const [stateValue, setStateValue] = useState<T>(
-    hasServerState ? getLsValue() : defaultValueRef.current,
-  );
+  const [stateValue, setStateValue] = useState<T>(defaultValueRef.current);
 
   useEffect(() => {
     const value = getLsValue();
